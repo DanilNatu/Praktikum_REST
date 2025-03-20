c161be0d35dc5fc57e9a52de101b5adfbd50c7ce
Denilson <natudenilson@gmail.com>
2025-03-21 00:28:23 +0800

    Langkah pertama pembuat user-api.yaml. Membuat database pada Mysql dengan nama database openapi. setelah menginstall Node.js. Pada VsCode dimulai dengan membuat file index.js. Lalu pada terminal install beberapa library sebagai berikut install Express , menginstalasi library swagger ui pada terminal dan terakhir mengintal library yaml. Setelah melakukan intallasi, dapat dimulai dengan codingan pada index.js sebagai pembuatan open api dengan express dengan beberapa bagian yaitu get untuk memunculkan semua user, post untuk menambahkan user, get user by id untuk memunculkan user dengan id yang dipilih, dan delete untuk menghapus user berdasarkan id yang dipilih.

---
+++ index.js
@@ -0,0 +1,67 @@
+import express from 'express';
+import mysql from 'mysql2';
+import swaggerUi from 'swagger-ui-express';
+import fs from 'fs';
+import YAML from 'yaml';
+
+const swaggerDocument = YAML.parse(fs.readFileSync('./user-api.yaml', 'utf8'));
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    database: "openapi",
+    password: "",
+    port: "3307"
+});
+const app = express();
+
+app.use(express.json());
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get('/users', (req, res) => {
+    db.query('SELECT * FROM user', (err, results) => {
+        if (err) {
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+
+        res.json(results);
+    });
+});
+
+app.post('/user', (req, res) => {
+    const { name, email, age } = req.body;
+
+    db.query('INSERT INTO user (name, email, age, createdAt, updatedAt) VALUES (?, ?, ?, Now(), Now())',[name, email, age], (err, results) => {
+        if (err) {
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+
+        res.json(results);
+    });
+});
+
+app.get('/user/:id', (req, res) => {
+    db.query('SELECT * FROM user WHERE id = ?',[req.params.id], (err, results) => {
+        if (err) {
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+
+        res.json(results);
+    });
+});
+
+app.delete('/user/:id', (req, res) => {
+    db.query('DELETE FROM user WHERE id = ?',[req.params.id], (err, results) => {
+        if (err) {
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+
+        res.json( {massage: 'User berhasil dihapus'});
+    });
+});
+app.listen(3000, () => console.log('Server berjalan di http://localhost:3000'));
